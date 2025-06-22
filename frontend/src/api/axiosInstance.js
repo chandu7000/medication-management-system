54ca8a5d39fd2828574796947f992179ea0f69f9
chandu7000 <nadimintichandu700@gmail.com>
2025-06-22 17:10:58 +0530

    Replace old code with new project

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 // Create instance with base URL
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001/api', // Adjust if needed
+  baseURL: 'https://medication-backend-5.onrender.com/api', // Adjust if needed
   headers: {
     'Content-Type': 'application/json',
   },
